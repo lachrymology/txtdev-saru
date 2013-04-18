26985fddac7aae820b4014eb6e638d6e2db2c07e
fogus <mefogus@gmail.com>
2013-04-18 08:24:11 -0400

    Properties

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Saru")]
+[assembly: AssemblyDescription("A simple text editor sans distraction.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ariadne Softworks")]
+[assembly: AssemblyProduct("Saru")]
+[assembly: AssemblyCopyright("Copyright © 2009 Michael Fogus")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("22343311-f659-46dc-8218-431440698720")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
